bf6518be6194e7eb39b59ba6fe8095b153426a9f
Mohammed Che <mocherbatji@gmail.com>
2024-03-11 10:57:27 +0000

    Added user object

--- exercises/week2/objects/objects.js
+++ exercises/week2/objects/objects.js
@@ -1,7 +1,18 @@
-
 let myCar = {
     make: 'Ford',
     model: 'Mustang',
     year: 2024,
     options: ['HUD', 'Heated seats']
+};
+
+let user = {
+    firstName: 'John',
+    lastName: 'Jones',
+    age: 25,
+    hobbies: ['Gym', 'Movies'],
+    friends: {
+        firstName: 'Tom',
+        lastName: 'Murphy',
+        age: 27
+    }
 };
